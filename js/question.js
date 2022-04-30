78212f7f9ea1504126450f121c61683f021ee7d8
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-04-30 13:29:53 +0200

    screenshot

--- js/question.js
+++ js/question.js
@@ -2,32 +2,32 @@
 
 const quiz = [
     {
-        q:'What is capital city of USA',
+        q:'What is the capital city of USA',
         options:['Washington D.C','New York','Los Angeles','Huston'],
         answer:0
     },
     {
-        q:'What is capital city of Rusia',
+        q:'What is the capital city of Rusia',
         options:['St. Petersburg','Moska','Minsk','Kiev'],
         answer:1
     },
     {
-        q:'What is capital city of Thailand',
+        q:'What is the capital city of Thailand',
         options:['Bangkok','Tokyo','Being','Jakarta'],
         answer:0
     },
     {
-        q:'What is capital city of Libya',
+        q:'What is the capital city of Libya',
         options:['Cairo','Surt','Tripoli','Islamabad'],
         answer:2
     },
     {
-        q:'What is capital city of Argentina',
+        q:'What is the capital city of Argentina',
         options:['Lima','Bogota','Buenos Aires','Rosario'],
         answer:2
     },
     {
-        q:'What is capital city of Norway',
+        q:'What is the capital city of Norway',
         options:['Helsinki','Stockholm','Tromso','Osl0'],
         answer:3
     }
